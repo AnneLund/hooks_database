46cab5367f996566b6b5e888d5f6dd9f4269bf8b
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-09-20 07:04:27 +0200

    idx

--- pages/api/school.js
+++ pages/api/school.js
@@ -10,34 +10,34 @@ async function handler(req, res) {
     })
 
     if(req.method === "PUT") {
-        const {id, title} = req.body
-        if(!id || !title){
+        const {idx, title} = req.body
+        if(!idx || !title){
             res.status(422).json({message: "Invalid data"})
             return;
         }
-await executeQuery(`UPDATE school SET title = '${title}' WHERE id = '${id}'`)
+await executeQuery(`UPDATE school SET title = '${title}' WHERE id = '${idx}'`)
 res.status(201).json({message: "Data deleted!"})
 return;
     }
 
     if(req.method === "DELETE") {
-        const {id} = req.body
-        if(!id){
+        const {idx} = req.body
+        if(!idx){
             res.status(422).json({message: "Invalid data"})
             return;
         }
-await executeQuery(`DELETE FROM school WHERE id = '${id}'`)
+await executeQuery(`DELETE FROM school WHERE id = '${idx}'`)
 res.status(201).json({message: "Data deleted!"})
 return;
     }
 
 if(req.method === "POST") {
-    const {id} = req.body
-    if(!id){
+    const {idx} = req.body
+    if(!idx){
         res.status(422).json({message: "Invalid data"})
         return;
     }
-    const data = await executeQuery(`INSERT INTO school(id) VALUES('${id}')`)
+    const data = await executeQuery(`INSERT INTO school(id) VALUES('${idx}')`)
     res.status(201).json({message: "Data created!", data})
     return;
 }
